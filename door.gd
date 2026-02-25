940294bbdcbeaff3b1711536b1fe013d8f23f5ce
Games4Doritos <evanmiocevich@gmail.com>
2026-02-25 22:50:57 +0800

    Added basic door animation + resaved scenes
    - Added basic animation for entering a door, where the door will open and the next level will be routed to in 1 second
    - Resaved all scenes in the project, as the new version of Godot adds new attributes in their tscn files

--- door.gd
+++ door.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @export var connectedRoom = 1
 var playerTouching = false
+@onready var doorSprite = $AnimatedSprite2D
+@onready var enterTimer = $enterTimer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,4 +20,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("e") and playerTouching:
-		get_tree().change_scene_to_file("res://levels/level" + str(connectedRoom) + ".tscn")
+		doorSprite.play("brickOpen")
+		enterTimer.start()
+
+func _on_enter_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://levels/level" + str(connectedRoom) + ".tscn")
